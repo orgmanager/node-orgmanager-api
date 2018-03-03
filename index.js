3099494b99a9294fd405cf30710c6d1a1ec3fecf
TiagoDanin <TiagoDanin@outlook.com>
2018-03-03 18:27:38 -0300

    Add class's

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const request = require('request-promise-native')
 
 const baseUrl = 'https://orgmanager.miguelpiedrafita.com'
 
-
 function makeOptions(token, method, uri, formData) {
-	console.log(token)
+	if (token == false) {
+		throw new Error('token not found!')
+	}
 	var options = {
 		method: method,
 		baseUrl: baseUrl,
@@ -16,75 +17,81 @@ function makeOptions(token, method, uri, formData) {
 		formData: formData,
 		encoding: null,
 		transform: function(body, response, resolveWithFullResponse) {
-			//console.log(body)
 			var data = JSON.parse(body)
-			console.log(data)
 			return data
 		}
 	}
 	return options
 }
 
-async function getRoot() {
-	var options = makeOptions('GET', '/api', {})
-	return await request(options)
-}
-//getRoot()
+class orgmanager {
+	constructor(token) {
+		this.token = token || false
+	}
 
-async function getStats() {
-	var options = makeOptions('GET', '/api/stats', {})
-	return await request(options)
-}
-//getStats()
+	async getRoot() {
+			var options = makeOptions(this.token, 'GET', '/api', {})
+			return await request(options)
+		}
+		//getRoot()
 
-async function getUser() {
-	var options = makeOptions('GET', '/api/user', {})
-	return await request(options)
-}
-//getUser()
+	async getStats() {
+			var options = makeOptions(this.token, 'GET', '/api/stats', {})
+			return await request(options)
+		}
+		//getStats()
 
-async function getOrgs() {
-	var options = makeOptions('GET', '/api/user/orgs', {})
-	return await request(options)
-}
-//getOrgs()
+	async getUser() {
+			var options = makeOptions(this.token, 'GET', '/api/user', {})
+			return await request(options)
+		}
+		//getUser()
 
-async function getOrg(id) {
-	var options = makeOptions('GET', `/api/org/${id}`, {})
-	return await request(options)
-}
-//getOrg(id)
+	async getOrgs() {
+			var options = makeOptions(this.token, 'GET', '/api/user/orgs', {})
+			return await request(options)
+		}
+		//getOrgs()
 
-async function regenerateToken() {
-	var options = makeOptions('GET', '/api/token/regenerate', {})
-	return await request(options)
-}
-//regenerateToken()
+	async getOrg(id) {
+			var options = makeOptions(this.token, 'GET', `/api/org/${id}`, {})
+			return await request(options)
+		}
+		//getOrg(id)
 
-async function changeOrgPassword(id, password) {
-	var options = makeOptions('POST', `/api/org/${id}`, {
-		password: password
-	})
-	return await request(options)
-}
-//changeOrgPassword(id, password)
+	async regenerateToken() {
+			var options = makeOptions(this.token, 'GET', '/api/token/regenerate', {})
+			return await request(options)
+		}
+		//regenerateToken()
 
-async function joinOrg(id, username) {
-	var options = makeOptions('POST', `/api/join/${id}`, {
-		username: username
-	})
-	return await request(options)
-}
-//joinOrg(id, username)
+	async changeOrgPassword(id, password) {
+			var options = makeOptions(this.token, 'POST', `/api/org/${id}`, {
+				password: password
+			})
+			return await request(options)
+		}
+		//changeOrgPassword(id, password)
 
-async function updateOrg(id) {
-	var options = makeOptions('PUT', `/api/org/${id}`, {})
-	return await request(options)
-}
-//updateOrg(id)
+	async joinOrg(id, username) {
+			var options = makeOptions(this.token, 'POST', `/api/join/${id}`, {
+				username: username
+			})
+			return await request(options)
+		}
+		//joinOrg(id, username)
 
-async function deleteOrg(id) {
-	var options = makeOptions('DELETE', `/api/org/${id}`, {})
-	return await request(options)
+	async updateOrg(id) {
+			var options = makeOptions(this.token, 'PUT', `/api/org/${id}`, {})
+			return await request(options)
+		}
+		//updateOrg(id)
+
+	async deleteOrg(id) {
+			var options = makeOptions(this.token, 'DELETE', `/api/org/${id}`, {})
+			return await request(options)
+		}
+		//deleteOrg(id)
 }
-//deleteOrg(id)
+
+module.exports = orgmanager
